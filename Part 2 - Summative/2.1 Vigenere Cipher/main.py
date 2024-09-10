e07e049e9c86d071e94d9f420860b66228bb3368
191261 <2025miller.hana@fcboe.org>
2024-09-10 11:08:25 -0400

    wrote tests for vig_encode

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -1,15 +1,43 @@
 # Read the instructions to see what you need to do here!
 
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
 
 
 def vig_encode(text, keyword):
+    """
+    Encodes given text with a vigenere cipher using the given keyword.
+
+    Spaces are read and returned as underscores.
+    Special characters and numbers in the text are omitted.
+    The encoded text will be all capital letters regardless of original capitalization.
+    :param text:
+    :param keyword:
+    :return:
+    """
+
     encoded_word = ""
-    for i in range(len(text)):
+    simplified_text = ""
+    simplified_keyword = ""
+    text = str(text)
+
+    for letter in text:
+        if letter.isalpha():
+            simplified_text += letter.upper()
+        elif letter == "_" or letter == " ":
+            simplified_text += "_"
+
+    for letter in keyword:
+        if letter.isalpha():
+            simplified_text += letter.upper()
+        elif letter == "_" or letter == " ":
+            simplified_text += "_"
+
+    for i in range(len(simplified_text)):
         text_index = alpha.index(text[i])
         keyword_index = alpha.index(keyword[i % len(keyword)])
         encoded_index = keyword_index + text_index
         encoded_word += alpha[encoded_index % len(alpha)]
+
     return encoded_word
 
 
--- Part 2 - Summative/2.1 Vigenere Cipher/tests/test_vig_encode.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/tests/test_vig_encode.py
@@ -6,19 +6,49 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from main import vig_encode
 class TestVigEncode(TestCase):
     def test_vig_encode_lowercase_text_and_keyword(self):
-        self.assertEqual(vig_encode("hello", "test"), "aideh")
+        self.assertEqual(vig_encode("hello", "test"), "_ICDG")
 
     def test_vig_encode_uppercase_text_and_keyword(self):
-        self.assertEqual(vig_encode("hello".upper(), "test".upper()), "aideh".upper())
+        self.assertEqual(vig_encode("hello".upper(), "test".upper()), "_ICDG")
 
     def test_vig_encode_lowercase_text_uppercase_keyword(self):
-        self.assertEqual(vig_encode("hello", "test".upper()), "aideh")
+        self.assertEqual(vig_encode("hello", "test".upper()), "_ICDG")
 
     def test_vig_encode_uppercase_text_lowercase_keyword(self):
-        self.assertEqual(vig_encode("hello".upper(), "test"), "aideh".upper())
+        self.assertEqual(vig_encode("hello".upper(), "test"), "_ICDG")
+
+    def test_vig_encode_uppercase_and_lowercase_keyword(self):
+        self.assertEqual(vig_encode("hello", "Test"), "_ICDG")
+
+    def test_vig_encode_uppercase_and_lowercase_text(self):
+        self.assertEqual(vig_encode("heLlo", "test"), "_ICDG")
+
+    def test_vig_encode_long_keyword(self):
+        self.assertEqual(vig_encode("hello", "pneumonoultramicroscopicsilicovolcanoconiosis"), "WRPE_")
+
+    def test_vig_encode_one_letter_keyword(self):
+        self.assertEqual(vig_encode("hello", "h"), "OLSSV")
+
+    def test_vig_encode_number_in_keyword(self):
+        self.assertEqual(vig_encode("hello", "1test2test"), "_ICDG")
+
+    def test_vig_encode_special_chars_in_keyword(self):
+        self.assertEqual(vig_encode("hello", "!test?test"), "_ICDG")
 
     def test_vig_encode_numbers(self):
-        self.assertEqual(vig_encode("123", "encode"), "aideh".upper())
+        self.assertEqual(vig_encode("123", "encode"), "")
+
+    def test_vig_encode_special_chars(self):
+        self.assertEqual(vig_encode("!@#$%^&*()", "encode"), "")
+
+    def test_vig_encode_blank(self):
+        self.assertEqual(vig_encode("", "encode"), "")
+
+    def test_vig_encode_space(self):
+        self.assertEqual(vig_encode(" ", "encode"), "D")
+
+    def test_vig_encode_sentence(self):
+        self.assertEqual(vig_encode("Hello, my name is Hana! I am in 12th grade.", "encode"), "LRNZRDQKBADQIMKFCLE_CNLDEZBWQDXUBUUEHR")
 
 
 
